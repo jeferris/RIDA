9b767ba4dae0ba5b7c005fa534e2c7afba00569c
jeferris <jeferris@crimson.ua.edu>
2020-02-24 15:56:55 -0600

    error checking on create new user

--- RIDA/app/src/main/java/com/example/rida/NewUser.java
+++ RIDA/app/src/main/java/com/example/rida/NewUser.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,20 +34,32 @@ public class NewUser extends AppCompatActivity {
                 TextView email_newusr = (TextView) findViewById(R.id.email_newusr);
                 TextView password_newusr = (TextView) findViewById(R.id.password_newusr);
                 TextView confirm_pass_newusr = (TextView) findViewById(R.id.conf_password_newusr);
+                CheckBox lyft_newusr = (CheckBox) findViewById(R.id.lyft_newusr);
+                CheckBox uber_newusr = (CheckBox) findViewById(R.id.uber_newusr);
 
                 String name = full_name_newusr.getText().toString();
                 String phone = phone_newusr.getText().toString();
                 String email = email_newusr.getText().toString();
                 String password = password_newusr.getText().toString();
                 String conf_pass = confirm_pass_newusr.getText().toString();
+                boolean lyft = lyft_newusr.isChecked();
+                boolean uber = uber_newusr.isChecked();
 
-                if ( name == "" || phone == "" || email == "" || password == "" || conf_pass == "") {
+                if (    name.compareTo("") == 0 ||
+                        phone.compareTo("") == 0 ||
+                        email.compareTo("") == 0 ||
+                        password.compareTo("") == 0 ||
+                        conf_pass.compareTo("") == 0
+                ) {
                     Toast.makeText(v.getContext(),"All fields are required.", Toast.LENGTH_LONG+4).show();
                 }
-                else if (password != conf_pass) {
+                else if (password.compareTo(conf_pass) != 0) {
                     Toast.makeText(v.getContext(),"Password and Confirm Password must match.", Toast.LENGTH_LONG+4).show();
                     password_newusr.setText("");
                     confirm_pass_newusr.setText("");
+                }
+                else if (!lyft && !uber) {
+                    Toast.makeText(v.getContext(),"You must select either Uber, Lyft, or both.", Toast.LENGTH_LONG+4).show();
                 }
                 else {
                     Intent createNewUsrIntent = new Intent(getApplication(), HomeActivity.class);
