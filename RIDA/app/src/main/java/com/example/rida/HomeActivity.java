739dc6e2fdc43f8561cf9b26e11e0efc1034662a
jeferris <jeferris@crimson.ua.edu>
2020-02-24 18:44:37 -0600

    workng on navigation bar

--- RIDA/app/src/main/java/com/example/rida/HomeActivity.java
+++ RIDA/app/src/main/java/com/example/rida/HomeActivity.java
@@ -3,12 +3,18 @@ package com.example.rida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.article_view, container, false);
     }
 }
