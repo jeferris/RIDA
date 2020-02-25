9108345f5ab5144b9aeb3e729561984e35972959
avevans <avevans@crimson.ua.edu>
2020-02-25 13:37:46 -0600

    changes

--- RIDA/app/src/main/java/com/example/rida/NewUser.java
+++ RIDA/app/src/main/java/com/example/rida/NewUser.java
@@ -9,14 +9,98 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import android.text.TextUtils;
+
+
 
 public class NewUser extends AppCompatActivity {
+    private EditText emailTV, passwordTV, full_nameTV, phone_numberTV, confirm_pass_newusrTV;
+    private Button regBtn;
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        initializeUI();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerNewUser();
+            }
+        });
+    }
+
+    private void registerNewUser() {
+
+
+        String email, password, full_name, phone_number, confirm_pass;
+        email = emailTV.getText().toString();
+        password = passwordTV.getText().toString();
+        full_name = full_nameTV.getText().toString();
+        phone_number = phone_numberTV.getText().toString();
+        confirm_pass = confirm_pass_newusrTV.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(full_name)) {
+            Toast.makeText(getApplicationContext(), "Please enter full name!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(phone_number)) {
+            Toast.makeText(getApplicationContext(), "Please enter phone number!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(confirm_pass)) {
+            Toast.makeText(getApplicationContext(), "Please confirm password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
+
+
+                            Intent intent = new Intent(NewUser.this, SignIn.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+    }
+
+    private void initializeUI() {
+        emailTV = findViewById(R.id.email_newusr);
+        passwordTV = findViewById(R.id.password_newusr);
+        regBtn = findViewById(R.id.create_acct_newusr);
+        full_nameTV = findViewById(R.id.full_name_newusr);
+        phone_numberTV = findViewById(R.id.phone_newusr);
+        confirm_pass_newusrTV = findViewById(R.id.conf_password_newusr);
 
         ImageButton back_btn_newusr = findViewById(R.id.back_btn_newusr);
         back_btn_newusr.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +110,29 @@ public class NewUser extends AppCompatActivity {
             }
         });
 
+        Button sign_in_newusr = findViewById(R.id.sign_in_newusr);
+        sign_in_newusr.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent signInNewUsrIntent = new Intent(getApplication(), SignIn.class);
+                startActivity(signInNewUsrIntent);
+            }
+        });
+    }
+}
+
+        //firebase
+        /*
+
+
+
+        ImageButton back_btn_newusr = findViewById(R.id.back_btn_newusr);
+        back_btn_newusr.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent backNewUsrIntent = new Intent(getApplicationContext(), MainActivity.class );
+                startActivity(backNewUsrIntent);
+            }
+        });
+/*
         Button create_acct_newusr = findViewById(R.id.create_acct_newusr);
         create_acct_newusr.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -62,12 +169,16 @@ public class NewUser extends AppCompatActivity {
                     Toast.makeText(v.getContext(),"You must select Uber, Lyft, or both.", Toast.LENGTH_LONG+4).show();
                 }
                 else {
+                    //saves data to database
+
+                    //saves data to database
+
                     Intent createNewUsrIntent = new Intent(getApplication(), HomeActivity.class);
                     startActivity(createNewUsrIntent);
                 }
             }
-        });
-
+        });*/
+/*
         Button sign_in_newusr = findViewById(R.id.sign_in_newusr);
         sign_in_newusr.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
@@ -77,3 +188,5 @@ public class NewUser extends AppCompatActivity {
         });
     }
 }
+
+ */
--- RIDA/app/src/main/java/com/example/rida/SignIn.java
+++ RIDA/app/src/main/java/com/example/rida/SignIn.java
@@ -6,6 +6,110 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import android.text.TextUtils;
+
+
+public class SignIn extends AppCompatActivity {
+
+    private EditText emailTV, passwordTV;
+    private Button loginBtn;
+
+
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        initializeUI();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUserAccount();
+            }
+        });
+    }
+
+    private void loginUserAccount() {
+
+
+        String email, password;
+        email = emailTV.getText().toString();
+        password = passwordTV.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+
+
+                            Intent intent = new Intent(SignIn.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+    }
+
+    private void initializeUI() {
+        emailTV = findViewById(R.id.email_signin); //will be email for now
+        passwordTV = findViewById(R.id.password_signin);
+        loginBtn = findViewById(R.id.sign_in_signin);
+
+        ImageButton back_btn_signin = findViewById(R.id.back_btn_signin);
+        back_btn_signin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent backSignInIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(backSignInIntent);
+            }
+        });
+
+        Button new_acct_signin = findViewById(R.id.new_acct_signin);
+        new_acct_signin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent newAcctSigninIntent = new Intent(getApplication(), NewUser.class);
+                startActivity(newAcctSigninIntent);
+            }
+        });
+    }
+}
+/*
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -57,3 +161,5 @@ public class SignIn extends AppCompatActivity {
         });
     }
 }
+
+ */
