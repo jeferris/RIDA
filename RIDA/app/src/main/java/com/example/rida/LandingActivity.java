c30741be4186016878b23461f20b1e0a17534555
jeferris <jeferris@crimson.ua.edu>
2020-03-03 14:12:35 -0600

    navigation bar stuff

--- RIDA/app/src/main/java/com/example/rida/LandingActivity.java
+++ RIDA/app/src/main/java/com/example/rida/LandingActivity.java
@@ -20,7 +20,7 @@ public class LandingActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_events, R.id.navigation_expenses, R.id.navigation_hotspots, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
