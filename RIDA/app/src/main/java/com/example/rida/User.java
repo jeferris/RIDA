e470ce9f1d534fea77d40ff80974cda4f6c6a6e9
cwefelmeyer <cwwefelmeyer@crimson.ua.edu>
2020-04-21 17:22:27 -0500

    Updates User.java and NewUser.java to add user info to database

--- RIDA/app/src/main/java/com/example/rida/User.java
+++ RIDA/app/src/main/java/com/example/rida/User.java
@@ -1,10 +1,10 @@
 package com.example.rida;
 
 public class User {
-    public String full_name;
-    public String email;
-    public String password;
-    public String phone_number;
+    private String full_name;
+    private String email;
+    private String password;
+    private String phone_number;
 
 
     public User() {
@@ -12,6 +12,48 @@ public class User {
         this.email = "sample@email.com";
         this.password = "password";
         this.phone_number = "1234567890";
+    }
+
+
+
+    public User(String full_name, String email, String password, String phone_number)
+    {
+        this.full_name = full_name;
+        this.email = email;
+        this.password = password;
+        this.phone_number = phone_number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFull_name(String full_name){
+        this.full_name = full_name;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setPhone_number(String phone_number){
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
     }
 }
 //public User()
